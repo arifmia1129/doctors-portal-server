123c8d15e00edec1d5b56b15ebe9ea3391252ce7
arifvtti <arif.vtti@gmail.com>
2022-05-15 23:30:05 +0600

    make api that added admin role in user info

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const verifyJWT = (req, res, next) => {
             return res.status(401).send({ message: "Forbidden access!" })
         }
         req.decoded = decoded;
-        console.log(decoded);
         next();
     })
 }
@@ -88,6 +87,22 @@ async function run() {
 
         })
 
+        app.get("/user", verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+        app.put("/user/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const filter = { email };
+            console.log(filter);
+            const updateDoc = {
+                $set: { role: "admin" },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            console.log(filter, updateDoc, result);
+            res.send(result);
+        })
         app.put("/user/:email", async (req, res) => {
             const email = req.params.email;
             const user = req.body;
